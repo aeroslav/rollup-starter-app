322ea00395c74d0f8967f65e9b17425ee4a542d5
Yaraslau Sauchanka <yaraslau.sauchanka@Kronos.com>
2019-04-02 16:23:16 +0300

    Add html template plugin; build folder can be completely removed

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import htmlTemplate from 'rollup-plugin-generate-html-template';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
-	},
-	plugins: [
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
-	]
+    input: 'src/main.js',
+    output: {
+        file: 'build/bundle.js',
+        format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+        sourcemap: true
+    },
+    plugins: [
+        resolve(), // tells Rollup how to find date-fns in node_modules
+        commonjs(), // converts date-fns to ES modules
+        htmlTemplate({
+            template: 'src/index.html',
+            target: 'index.html',
+        }),
+        production && terser() // minify, but only in production
+    ]
 };
--- src/update.js
+++ src/update.js
@@ -3,6 +3,6 @@ import format from 'date-fns/format';
 var span = document.querySelector('#time-now');
 
 export default function update() {
-	span.textContent = format(new Date(), 'h:mm:ssa');
-	setTimeout(update, 1000);
+    span.textContent = format(new Date(), 'h:mm:ssa');
+    setTimeout(update, 1000);
 }
